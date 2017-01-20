80714926cb445eae56eab161a8bae1b4f082f8c5
darryl liew <darryl.liew@elasticpath.com>
2017-01-20 15:53:39 -0800

    cleaning up code

--- Assignment1/images.js
+++ Assignment1/images.js
@@ -1,15 +1,13 @@
 $( "select" ).change(function () {
-  var str = "";
-
   $( "select option:selected" ).each(function() {});
 
-  if($( this ).val() == "downtown") {    
-    $("#description").html('<p>The BCIT Downtown Campus is in the heart of Vancouver, near the Granville Skytrain Station.</p><p>555 Seymour Street<br />Vancouver, British Columbia<br />V6B 3H6 , Canada</p>Web link: <a href="http://www.bcit.ca/about/downtown.shtml">Downtown Campus</a>');
+  if( $( this ).val() == "downtown" ) {    
+    $("#description").html('<p>The BCIT Downtown Campus is in the heart of Vancouver, near the Granville Skytrain Station.</p><p>555 Seymour Street<br />Vancouver, British Columbia<br />V6B 3H6 , Canada</p>More Information: <a href="http://www.bcit.ca/about/downtown.shtml">Downtown Campus</a>');
     $("figure").html('<img style="height:100%;width:100%" src="1.jpg" />')    
   }
 
-  if($( this ).val() == "burnaby") {
-    $("#description").html('<p>The BCIT Burnaby Campus is located just off the Trans-Canada Highway at the corner of Willingdon Avenue and Canada Way</p><p>3700 Willingdon Avenue <br />Burnaby, BC V5G 3H2</p>Web link: <a href="http://www.bcit.ca/about/burnaby.shtml">Burnaby Campus</center></a>');
+  if( $( this ).val() == "burnaby" ) {
+    $("#description").html('<p>The BCIT Burnaby Campus is located just off the Trans-Canada Highway at the corner of Willingdon Avenue and Canada Way</p><p>3700 Willingdon Avenue <br />Burnaby, BC V5G 3H2</p>More Information: <a href="http://www.bcit.ca/about/burnaby.shtml">Burnaby Campus</center></a>');
     $("figure").html('<img style="height:100%;width:100%" src="2.jpg" />')    
   }
 
